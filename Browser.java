ff9d567e6d78a68a87909cc242b479064e653a15
Poornima-Devi12 <147127197+Poornima-Devi12@users.noreply.github.com>
2024-02-13 23:02:15 +0530

    Update Browser.java

--- Browser.java
+++ Browser.java
@@ -8,17 +8,21 @@ public class Browser {
 	public static void main(String[] args) {
 		
 		
-				       // open the browser and launch the google url
-						//set the driver
-					ChromeDriver driver=new ChromeDriver();
-					   // add the Url
-					driver.get("https://www.google.com/");
-						// maximize the driver
-					driver.manage().window().maximize();
-						//locate the element
-					driver.findElement(By.name("csi")).sendKeys("Selenium Browser Driver",Keys.ENTER);
-						// close the browser
-					driver.close();
+	// open the browser and launch the google url
+	//set the driver
+	ChromeDriver driver=new ChromeDriver();
+	
+	// add the Url
+	driver.get("https://www.google.com/");
+						
+	// maximize the driver
+	driver.manage().window().maximize();
+						
+	//locate the element
+	driver.findElement(By.name("csi")).sendKeys("Selenium Browser Driver",Keys.ENTER);
+						
+	// close the browser
+	driver.close();
 
 	}
 
